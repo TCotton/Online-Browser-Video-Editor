b4ca0e638f950a7145921e6620bf4b4115488688
Andy Walpole <andy.walpole@gmail.com>
2021-03-09 09:08:36 +0000

    added useHotReload hook

--- src/components/new/PlayerComponents/D3BarChart.jsx
+++ src/components/new/PlayerComponents/D3BarChart.jsx
@@ -1,37 +1,62 @@
 import * as d3 from "d3";
 import React, {useEffect, useRef} from "react";
+import useHotReload from '../../../hooks/useHotReload'
 
 const D3BarChart = props => {
     const {data, width, height, padding} = props;
+    useHotReload();
     const ref = useRef(null);
 
-    const svg = d3.select(ref.current);
+    const svg = d3.select(ref.current)
+    svg.append("g").attr('class', 'barChart');
 
     useEffect(() => {
         if (data && ref.current) {
 
-            // Bind D3 data
-            const update = svg
-                .append('g')
-                .selectAll('text')
-                .data(data);
+            const colour = `hsla(216, 8, 12, 1)`;
+            const result = [data, colour];
+            console.dir(result);
 
-            // Enter new D3 elements
-            update.enter()
-                .append('text')
-                .attr('x', (d, i) => i * 25)
-                .attr('y', 40)
-                .style('font-size', 24)
-                .text((d) => d);
+            //svg.selectAll('.barChart').selectAll("*").remove();
 
-            // Update existing D3 elements
-            update
-                .attr('x', (d, i) => i * 40)
-                .text((d) => d);
+            const x = d3.scaleBand().range([0, width]).domain(result.map(d => d[1])).padding(padding);
+            const y = d3.scaleLinear().range([height, 0]).domain([1, 255]);
 
-            // Remove old D3 elements
-            update.exit()
-                .remove();
+            const elm = svg.selectAll('.barChart').selectAll('.rect').datum(data);
+
+            elm.join(
+                enter => {
+                    enter.append('rect')
+                        .attr('width', x.bandwidth)
+                        .attr("height", d => {
+                            if (d && d[0]) {
+                                console.log('create');
+                                return height - y(d[0]);
+                            }
+                            return 0;
+                        })
+                        .attr('fill', d => {
+                            if (d && d[1]) {
+                                console.log(d[1]);
+                                return d[1]
+                            }
+                            return null;
+                        })
+                        .attr('class', 'rect');
+                },
+                update => {
+                    update.attr("height", d => {
+                        if (d && d[0]) {
+                            console.log('update');
+                            return height - y(d[0]);
+                        }
+                        return null;
+                    })
+                },
+                destroy => {
+                    destroy.exit().remove();
+                }
+            )
         }
     }, [data, height, padding, width]);
 
