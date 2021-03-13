32cc94c619957d30dbc4902fe02be42997749460
Andy Walpole <andy.walpole@gmail.com>
2021-03-13 14:20:44 +0000

    working d3 barchart

--- src/components/new/PlayerComponents/BarChart.jsx
+++ src/components/new/PlayerComponents/BarChart.jsx
@@ -1,28 +1,64 @@
-import React, { useEffect, useRef } from "react";
+import React, {useEffect, useRef} from "react";
 import * as d3 from "d3";
 
 const BarChart = props => {
-    const { randomData, width, height, padding } = props;
-    const ref = useRef(null);
+    const {randomData, width, height, padding} = props;
 
-    const group = d3.select(ref.current);
+    const ref = useRef()
 
     useEffect(() => {
         const svgElement = d3.select(ref.current);
 
-        svgElement.selectAll('.rects').selectAll("rect")
-            .data(randomData)
-            .join("rect");
+        const colour = `hsla(216, 8, 12, 1)`;
+        const result = [{h:randomData, c:colour}];
 
+        const x = d3.scaleBand().range([0, width]).domain(result.map(d => d.c)).padding(padding);
+        const y = d3.scaleLinear().range([height, 0]).domain([1, 255]);
+
+        const svg = svgElement.selectAll('.rects').selectAll("rect").data(result);
+
+        svg.join(
+            enter => {
+                enter.append('rect')
+                    .attr('width', x.bandwidth)
+                    .attr("height", d => {
+                        console.log(d);
+                        if (d && d.h) {
+                            return height - y(d.h);
+                        }
+                        return 0;
+                    })
+                    .attr('fill', d => {
+                        console.log(d);
+                        if (d && d.c) {
+                            console.log(d.c);
+                            return d.c;
+                        }
+                        return null;
+                    })
+                    .attr('class', 'rect');
+            },
+            update => {
+                update.attr("height", d => {
+                    if (d && d.h) {
+                        return height - y(d.h);
+                    }
+                    return null;
+                })
+            },
+            destroy => {
+                destroy.exit().remove();
+            }
+        )
     }, [randomData])
 
     return (
-        <svg 
-            width={width} 
-            height={height} 
-            ref={ref} 
+        <svg
+            width={width}
+            height={height}
+            ref={ref}
         >
-            <g className="rects"></g>
+            <g className="rects"/>
         </svg>
     );
 };
--- src/components/new/PlayerComponents/ChannelDisplay.js
+++ src/components/new/PlayerComponents/ChannelDisplay.js
@@ -1,31 +1,12 @@
-import React, {useState, useCallback} from 'react';
+import React from 'react';
 import {useSelector} from "react-redux";
 import {peakFrequency} from '../slices/audioSlice';
 import BarChart from './BarChart';
 //import D3BarChart from './D3BarChart';
 import Circles from './AnimatedCircles';
 
-const Foo = () => {
-    const [ref, setRef] = useState(null);
-
-    const onRefChange = useCallback(node => {
-        // ref value changed to node
-        setRef(node); // e.g. change ref state to trigger re-render
-        if (node !== null) {
-            console.log('null');
-            // node is null, if DOM node of ref had been unmounted before
-        } else {
-            console.log('not null');
-            // ref value exists
-        }
-    }, );
-
-    return <h1 ref={onRefChange}>Hey</h1>;
-}
-
 const ChannelDisplay = () => {
     const range = useSelector(peakFrequency);
-    console.dir(range);
 
     return (
         <>
